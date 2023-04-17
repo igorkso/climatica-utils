00a969561f808d9572793fe83611fc623c973e39
Igor Oliveira <igor.oliveira@ccc.ufcg.edu.br>
2023-04-16 21:52:53 -0300

    working

---
+++ main.tf
@@ -0,0 +1,77 @@
+# Inicializando o terraform e indicando o provider do libvirt
+
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+}
+
+provider "libvirt" {
+   uri = "qemu:///system" #destino da vm
+}
+
+
+# Criando o volume onde todas as vms serão conectadas
+
+resource "libvirt_volume" "sda" {
+  name    = "${var.VOL_NAME}_${var.vms}"
+  pool    = "${var.POOL_NAME}"
+  source  = "${var.IMG_SOURCE}"
+  format  = "qcow2"
+}
+
+
+resource "libvirt_volume" "ubuntu-qcow2-resized" {
+
+  name           = "${var.VOL_NAME}_${var.vms}-resized.qcow2"
+  pool           = "${var.POOL_NAME}"
+  base_volume_id = libvirt_volume.sda.id
+  size           = var.disk
+  
+}
+
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/cloud_init.cfg")}"
+}
+
+resource "libvirt_cloudinit_disk" "cloud_init" {
+  name      = "commoninit_${var.vms}.iso"
+  pool      = "${var.POOL_NAME}"
+  user_data = "${data.template_file.user_data.rendered}"
+}
+
+resource "libvirt_domain" "ubuntu" {
+  name   = "${var.vms}"
+  memory = "${var.MEMORY_SIZE}"
+  vcpu   = "${var.VCPU_SIZE}"
+
+  network_interface {
+    network_name = "default"
+    hostname     = "${var.vms}"
+  }
+
+  disk {
+    volume_id = libvirt_volume.ubuntu-qcow2-resized.id
+  }
+
+  cloudinit = "${libvirt_cloudinit_disk.cloud_init.id}"
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,117 @@
+variable "POOL_NAME" {
+    type    = string
+    default = "all_volumes"
+
+}
+
+variable "VOL_NAME" {
+    type    = string
+    default = "sda1"
+
+}
+
+variable "VOL_DESTINY" {
+   type    = string
+   #default = "/storage/libvirt-volumes/"
+   default  = "/tfdata/pool_after"
+}
+
+variable "IMG_SOURCE" {
+   type    = string
+   #default = "/storage/ubuntu_core/focal-server-cloudimg-amd64.img"
+   default = "/tfdata/ubuntu-core/jammy-server-cloudimg-amd64.img"
+}
+
+variable "MEMORY_SIZE" {
+    type = string  
+    default = 1024*2 
+}
+variable "VCPU_SIZE" {
+    type = number
+    default = 2
+}
+
+variable "VM_USER" {
+    type = string 
+    default = "suporte"
+}
+
+variable "VM_HOSTNAME" {
+    type = string
+    default = "worker"
+}
+
+variable "VM_IMG_URL" {
+    type    = string
+    default = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+}
+variable "VM_VOLUME_SIZE" {
+    type    = number
+    default = 1024*1024*1024*10
+}
+
+variable "VM_IMG_FORMAT" {
+    type = string
+    default = "qcow2"
+}
+
+variable "VM_CIDR_RANGE" {
+    type = string
+    default = "192.168.1.0/24"
+}
+
+
+variable "ssh_key" {
+    type    = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDY8kTtEZrDAM9Q+E8quZuggErqXGOZCNKHvDt+L8RSQajHn1flZw0U1I+opV/OOlLTy0gy1IIJsqYHk4zyxKYMTevUwHxdaQB0BlAhMZMBD4pn4K6V+wrd16iBL07jk7YVBkAl18WS+c0v6PssarbEkDyHK3+jsnWjMIiQziP9wZf74JDCSwdtdcPSt0mcxdTyZ5DmkTJhoxlkflJyM/7m4AM6cABuBAFfubhCDDz/a3/90IacgJaFV1Vx1ZBf0/6fn1p1QH93XYo/aNyT40OcfhQPJ436LcsFrfRmozQiX/0oQGFQJ2o27TNhgbe15opQBS5hGgJh7bmorrRUMKZwz4yFfT5OwZ+TSXVSbIQtXWXWbl7CB52In6En2OzQWG3MVQxmXKE5hJ05ACte5pqdXSX+bEKJjjU/Eyovq9EqsRMkkphDtuc77sggCJngVq4H+je5jh/Epi87pwnzwapfKFdjHHAwHPyH3R0SSpBxyERZT8JAYrCqMe3F0qe0knE= igor.oliveira@lsd-pc"
+}
+
+variable "auth_access" {
+    type    = bool
+    default = true 
+}
+
+variable "vms" {
+    type    = string 
+    default = "VM34343434"
+}
+
+variable "network" {
+  type    = string
+  default = "network-name"
+}
+
+variable "interface" {
+  type    = string
+  default = "ens3"
+}
+
+variable "vms_address" {
+  type    = list
+  default = ["10.0.0.2", "10.0.0.5"]
+}
+
+variable "net_gateway" {
+  type    = string
+  default = "10.0.0.1"
+}
+
+variable "net_mask" {
+  type    = string
+  default = "24"
+}
+
+variable "dns_addresses" {
+  type    = list
+  default = ["1.1.1.1", "8.8.8.8"]
+}
+
+variable "disk" {
+  type    = number
+  default =  21474836480 #20G
+}
+
+variable "mac-address" {
+  type    = list
+  default = ["52:54:00:ee:3b:c1", "52:54:00:b6:e0:b2", "52:54:00:a7:b0:a9"]
+}
